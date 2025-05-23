927fe722b8053bf325f65f99920cd890871fc69f
Sasha2799-hub <alex0kuz99@gmail.com>
2025-05-23 15:12:43 +0300

    hw14

---
+++ hw14/file.js
@@ -0,0 +1,50 @@
+"use strict"
+const btnSwipeLeftRight = document.querySelectorAll('.swipe-btn');
+const images = [...document.querySelectorAll('.slide')];
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+const dots = document.querySelectorAll('.btn-circle');
+
+prevBtn.classList.add('btn-hide');
+
+btnSwipeLeftRight.forEach(button => {
+    button.addEventListener('click', function(event) {
+        const currentBtn = event.target.closest('.swipe-btn');
+        const currentIndex = images.findIndex(img => !img.classList.contains('img-hidden'));
+
+        if (currentBtn.classList.contains('next')) {
+            if (currentIndex < images.length - 1) {
+                images[currentIndex].classList.add('img-hidden')
+                images[currentIndex + 1].classList.remove('img-hidden')
+                prevBtn.classList.remove('btn-hide')
+
+                if (currentIndex + 1 === images.length - 1) {
+                    nextBtn.classList.add('btn-hide');
+                }
+            }
+        }
+
+        if (currentBtn.classList.contains('prev')) {
+            if (currentIndex > 0) {
+                images[currentIndex].classList.add('img-hidden')
+                images[currentIndex - 1].classList.remove('img-hidden')
+
+                nextBtn.classList.remove('btn-hide')
+                if (currentIndex - 1 === 0) {
+                    prevBtn.classList.add('btn-hide')
+                }
+            }
+        }
+    })
+})
+
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        const currentIndex = images.findIndex(img => !img.classList.contains('img-hidden'));
+        if (index !== currentIndex) {
+            images[currentIndex].classList.add('img-hidden');
+            images[index].classList.remove('img-hidden');
+        }
+    })
+})
