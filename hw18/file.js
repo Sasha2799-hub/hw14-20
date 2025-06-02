06f8b2e352ea744d9174654533513e840f3a92a4
Sasha2799-hub <alex0kuz99@gmail.com>
2025-06-02 17:26:15 +0300

    add hw18

---
+++ hw18/file.js
@@ -0,0 +1,21 @@
+const timerElement = document.querySelector('.timer');
+const startBtn = document.querySelector('.start');
+
+let seconds = 100
+let intervalId = null
+
+startBtn.addEventListener('click', () => {
+  if (intervalId !== null) return
+
+  intervalId = setInterval(() => {
+    const min = String(Math.floor(seconds / 60)).padStart(2, '0')
+    const sec = String(seconds % 60).padStart(2, '0')
+    timerElement.textContent = `${min}:${sec}`
+    seconds--
+
+    if (seconds < 0) {
+      clearInterval(intervalId)
+      intervalId = null
+    }
+  }, 1000)
+});
